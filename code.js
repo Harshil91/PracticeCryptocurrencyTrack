70c260396376a57ad0c820c6c9470c71bb20c6ae
Harshil J <harshiljasra@gmail.com>
2019-08-25 12:24:17 -0400

    Added automatic price updates

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 const COIN_INFO_ENDPOINT = 'https://min-api.cryptocompare.com/data/top/totalvol?limit=30&tsym=USD';
-const COIN_PRICE_ENDPOINT = 'https://min-api.cryptocompare.com/data/pricemulti?tsyms=USD&fsyms=';
+const COIN_PRICE_ENDPOINT = 'https://min-api.cryptocompare.com/data/pricemulti?tsyms=USD&api_key=5927a4aaf8b7d4f68a9eabc906e925276874447b1ff795e6e86685143d20bacb&fsyms=';
 
 class CryptoApp {
   constructor(){
@@ -12,26 +12,40 @@ class CryptoApp {
 
     Object.keys(this.coinData).forEach(coinKey => {
       let coin = this.coinData[coinKey];
-      let coinTableEntry = document.createElement('tr');
-      coinTableEntry.id = coin.Id;
+      let coinTableEntry = document.getElementById(coin.Id);
+      if(coinTableEntry){
+        let price = document.getElementById(coin.Id + '-price');
+        if(coin.Price != price.innerText){
+          if(coin.Price > price.innerText){
+            price.className = 'up flash-up';
+            setTimeout(() => price.classList.remove('flash-up'), 500);
+          } else {
+            price.className = 'down flash-down';
+            setTimeout(() => price.classList.remove('flash-down'), 500);
+          }
+        }
+        price.innerText = coin.Price;
+      } else {
+        coinTableEntry = document.createElement('tr');
+        coinTableEntry.id = coin.Id;
 
-      let image = document.createElement('img');
-      image.src = 'https://www.cryptocompare.com' + coin.ImageUrl;
-      image.classList.add('coin-image');
+        let image = document.createElement('img');
+        image.src = 'https://www.cryptocompare.com' + coin.ImageUrl;
+        image.classList.add('coin-image');
 
-      let name = document.createElement('td');
-      name.innerText = coin.FullName;
+        let name = document.createElement('td');
+        name.innerText = coin.FullName;
 
-      let price = document.createElement('td');
-      price.id = coin.Id = '-price';
-      price.innerText = coin.Price;
+        let price = document.createElement('td');
+        price.id = coin.Id = '-price';
+        price.innerText = coin.Price;
 
-      coinTableEntry.appendChild(image);
-      coinTableEntry.appendChild(name);
-      coinTableEntry.appendChild(price);
+        coinTableEntry.appendChild(image);
+        coinTableEntry.appendChild(name);
+        coinTableEntry.appendChild(price);
 
-      tableBody.appendChild(coinTableEntry);
-
+        tableBody.appendChild(coinTableEntry);
+      }
     });
   }
 
@@ -52,7 +66,7 @@ class CryptoApp {
         this.coinData[coin.CoinInfo.Name] = coin.CoinInfo;
       });
       this.updateCoinDataPrices();
-      setInterval(this.updateCoinDataPrices.bind(this), 6000);
+      setInterval(this.updateCoinDataPrices.bind(this), 5000);
     });
   }
 
