40de509f6dbe3386e4bca6a155db7db94da68e96
Harshil J <harshiljasra@gmail.com>
2019-08-25 10:10:15 -0400

    Added coin infor and price endpoints

---
+++ code.js
@@ -0,0 +1,13 @@
+const COIN_INFO_ENDPOINT = 'https://min-api.cryptocompare.com/data/top/totalvol?limit=30&tsym=USD';
+const COIN_PRICE_ENDPOINT = 'https://min-api.cryptocompare.com/data/pricemulti?tsyms=USD&api_key=5927a4aaf8b7d4f68a9eabc906e925276874447b1ff795e6e86685143d20bacb&fsyms=';
+
+class CryptoApp {
+  constructor(){
+    this.coinData = {};
+  }
+
+
+  start (){
+
+  }
+}
